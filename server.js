8cfa2fad1be1e8080a207648793cd2366d3df19f
Thiomark <thiotsoela@gmail.com>
2020-06-21 11:51:04 +0200

    Express Notes and Basics for creating an api

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/", function (req, res) {
+    //res.sendStatus(400)
+    res.status(400).json({ success: false, data: "page does not exist" });
+});
+
+app.get("/names", function (req, res) {
+    // res.send("<h1>Hello World</h1>");
+    // res.json({name: "Itumeleng"});
+
+    res.status(200).json({ success: true, name: "Itumeleng", id: 1 });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
