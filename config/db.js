51450aa8fa42a639a79d7d3d575ebfcfb4f87a70
Thiomark <thiotsoela@gmail.com>
2020-06-21 18:05:14 +0200

    Adding and Connecting to the Mongoose database

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+        useCreateIndex: true,
+    });
+
+    console.log(`Connected to the Mongo DB at: ${conn.connection.host}`);
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bootcames = require("./routes/bootcamps"); // files for routes
 const morgan = require("morgan");
+const connectDb = require("./config/db");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connecting to the database
+connectDB();
 
 const app = express();
 
@@ -18,6 +23,13 @@ app.use("/api/v1/bootcamps", bootcames);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
     console.log("The server has started on Port: " + PORT);
+});
+
+// Handile unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close serve & exit process
+    server.close(() => process.exit(1));
 });
