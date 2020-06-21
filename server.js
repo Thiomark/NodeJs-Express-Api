e5269e472ef51be483e29b265caab52e40896ce4
Thiomark <thiotsoela@gmail.com>
2020-06-21 13:36:41 +0200

    Creating routes for a Bootcamp Api

--- server.js
+++ server.js
@@ -6,16 +6,39 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/", function (req, res) {
-    //res.sendStatus(400)
-    res.status(400).json({ success: false, data: "page does not exist" });
+// Creating routes for our api
+// The V1 is for incase we decide to change the api, without affect the old one
+app.get("/api/v1/bootcamps", function (req, res) {
+    res.status(200).json({ success: true, message: "Getting all Bootcamps" });
 });
 
-app.get("/names", function (req, res) {
-    // res.send("<h1>Hello World</h1>");
-    // res.json({name: "Itumeleng"});
+// The :ID act like a varialble
+app.get("/api/v1/bootcamps/:id", function (req, res) {
+    res.status(200).json({
+        success: true,
+        message: `Getting Bootcamps ${req.params.id}`,
+    });
+});
 
-    res.status(200).json({ success: true, name: "Itumeleng", id: 1 });
+app.post("/api/v1/bootcamps/", function (req, res) {
+    res.status(200).json({
+        success: true,
+        message: `Adding Bootcamps`,
+    });
+});
+
+app.put("/api/v1/bootcamps/:id", function (req, res) {
+    res.status(201).json({
+        success: true,
+        message: `updating Bootcamp ${req.params.id}`,
+    });
+});
+
+app.delete("/api/v1/bootcamps/:id", function (req, res) {
+    res.status(200).json({
+        success: true,
+        message: `Deleting Bootcamp ${req.params.id}`,
+    });
 });
 
 const PORT = process.env.PORT || 5000;
