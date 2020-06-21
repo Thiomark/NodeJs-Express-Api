b33d8ab407f57bd65e5899d72a69da87757eba6d
Thiomark <thiotsoela@gmail.com>
2020-06-21 15:11:19 +0200

    Using external MIddleware: Morgan

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bootcames = require("./routes/bootcamps"); // files for routes
-const logger = require("./middleWare/logger");
+const morgan = require("morgan");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcames);
